e66c5b7bab8a4ab8f9f6b60c66430d0519add0b5
Azenet <azenet2594@gmail.com>
2022-02-06 21:39:31 -0600

    added scroll effect

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const navLink = document.querySelectorAll(".nav-link");
 const search = document.querySelector('.search');
 const btnSearch = document.querySelector('.btn-search');
 const inputSearch = document.querySelector('.search-input');
+// scroll
+const boxes = document.querySelectorAll('.box');
 
 btnMenu.addEventListener("click", () =>{
   navMenu.classList.toggle("active");
@@ -20,3 +22,20 @@ link.addEventListener('click', ()=>{
 btnSearch.addEventListener('click', () =>{
   search.classList.toggle('active');
 })
+
+// scroll
+window.addEventListener('scroll', checkBoxes);
+checkBoxes();
+
+function checkBoxes(){
+  const triggerBottom = window.innerHeight / 5 * 4;
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) {
+    box.classList.add('show')
+  } else{
+    box.classList.remove('show')
+  }
+  })
+  
+}
